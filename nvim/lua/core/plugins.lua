50a9544c432c7d6f2f78ddf4e4e79c8f2e401e2b
Yuriy Panov <yuriy.p@elmis-soft.com>
2024-06-10 19:17:05 +0600

    nvim

---
+++ nvim/lua/core/plugins.lua
@@ -0,0 +1,41 @@
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{
+         "nvim-neo-tree/neo-tree.nvim",
+         branch = "v3.x",
+         dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+	    "s1n7ax/nvim-window-picker"
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+          }
+        },
+	{ "ellisonleao/gruvbox.nvim",priority = 1000, config = true, opts = ...},
+	{ "catppuccin/nvim", name = "catppuccin" , priority = 1000 },
+	{ "nvim-treesitter/nvim-treesitter"},
+	{ "neovim/nvim-lspconfig" },
+	{
+         'akinsho/flutter-tools.nvim',
+         lazy = false,
+         dependencies = {
+          'nvim-lua/plenary.nvim',
+          'stevearc/dressing.nvim', -- optional for vim.ui.select
+        },
+        config = true,
+        }
+})
+
