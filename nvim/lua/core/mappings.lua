3ce210bf9e8ac2cabbd62f28d8f6606cfc72b9e6
Yuriy Panov <yuriy.p@elmis-soft.com>
2024-06-11 13:17:32 +0600

    nvim

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -3,9 +3,8 @@ vim.g.mapleader = " "
 -- Neo Tree
 vim.keymap.set('n','<leader>e',':Neotree float focus<CR>')
 vim.keymap.set('n','<leader>o',':Neotree float git_status<CR>')
- 
 
-vim.keymap.set('n','K',vim.lsp.buf.hover)
-vim.keymap.set('n','gd',vim.lsp.buf.definition)
-vim.keymap.set('n','ca',vim.lsp.buf.code_action)
+--vim.keymap.set('n','K',vim.lsp.buf.hover)
+--vim.keymap.set('n','gd',vim.lsp.buf.definition)
+--vim.keymap.set('n','ca',vim.lsp.buf.code_action)
 --vim.keymap.set('n','K',vim.lsp.buf.hover)
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,7 +2,6 @@ local lspconfig = require('lspconfig')
 lspconfig.tsserver.setup {}
 lspconfig.prismals.setup {}
 lspconfig.golangci_lint_ls.setup {}
--- lspconfig.dartls.setup {}
 lspconfig.lua_ls.setup {
    settings = {
      Lua = {
@@ -15,10 +14,10 @@ lspconfig.ruff_lsp.setup {}
 
 
 
---vim.keymap.set('n', '<leader>lD', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>lD', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
---vim.keymap.set('n', '<leader>ld', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<leader>ld', vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
@@ -29,9 +28,10 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
         local opts = {buffer = ev.buf}
-        --vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        --vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
         --vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
         -- vim.keymap
         --     .set('n', '<Leader>sa', vim.lsp.buf.add_workspace_folder, opts)
@@ -41,10 +41,12 @@ vim.api.nvim_create_autocmd('LspAttach', {
         --     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
         -- end, opts)
         -- vim.keymap.set('n', '<Leader>D', vim.lsp.buf.type_definition, opts)
-        --vim.keymap.set('n', '<Leader>lr', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', '<Leader>lr', vim.lsp.buf.rename, opts)
         --vim.keymap.set({'n', 'v'}, '<Leader>la', vim.lsp.buf.code_action, opts)
-        -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
         vim.keymap.set('n', '<Leader>lf',
                        function() vim.lsp.buf.format {async = true} end, opts)
+        vim.keymap.set('n', 'ca', vim.lsp.buf.code_action, opts)
+        --vim.keymap.set('x', 'ca', '<Cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
     end
 })
